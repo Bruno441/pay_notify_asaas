f407ac848a3035a4fb87d445aeb56d07c32edcac
Bruno <brunoferreiraj3@gmail.com>
2025-09-02 20:18:44 -0300

    feat: Subindo projeto modificado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
-import { PaymentReceivedModule } from './payment-received/payment-received.module';
-=======
-import { PaymentReceivedModule } from './payment/payment.module';
->>>>>>> e0d7d07 (subindo assets)
+import { PaymentReceivedModule } from './payment/payment-received.module';
+
 
 @Module({
   imports: [PaymentReceivedModule],
