a42f60c50c4235ec9d03a18e8a069ea912b36d6b
Bruno <brunoferreiraj3@gmail.com>
2025-10-21 19:54:36 -0300

    fix: retirando erros de app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentReceivedModule } from './payment/payment.module';
-import { PaymentRefundedController } from './payment/payment-refunded.controller';
-import { PaymentRefundedService } from './payment/payment-refunded.service';
 
 @Module({
   imports: [PaymentReceivedModule],
-  controllers: [AppController, PaymentRefundedController],
-  providers: [AppService, PaymentRefundedService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
