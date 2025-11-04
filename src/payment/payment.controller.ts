72fbff9faefe28c24f6858f253a98deda24839b6
Bruno Ferreira <100526804+Bruno441@users.noreply.github.com>
2025-11-03 21:40:24 -0300

    fix: Ajustando leitura de token

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -143,14 +143,11 @@ export class PaymentReceivedController {
   @Post('payment-received')
   @HttpCode(HttpStatus.OK)
   async handlePaymentWebhook(
-    @Body() payload: any,
-    @Headers() todosOsHeaders: any, // <-- Pega todos os headers
+    @Body() payload: any
   ) {
-    // Agora, 'todosOsHeaders' é um objeto com todos os cabeçalhos
-    // Lembre-se que eles vêm em letras minúsculas!
-    const asaasToken = todosOsHeaders['asaas-webhook-token'];
 
-    this.logger.log(todosOsHeaders)
+    const asaasToken = payload.accessToken;
+
     const TokenSecreto = process.env.ASAAS_WEBHOOK_SECRET;
     if (!TokenSecreto || asaasToken !== TokenSecreto) {
       this.logger.warn('Token do webhook Asaas inválido ou ausente.');
